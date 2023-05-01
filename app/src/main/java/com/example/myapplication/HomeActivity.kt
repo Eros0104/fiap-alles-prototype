66cde4a3c95563f1dfbecdc112e17dad6f50039a
Eros Mendonça <erosmendonca@outlook.com>
2023-05-01 02:25:53 +0100

    feat: linking all activities 🔗

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,11 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnStatement = findViewById<Button>(R.id.btnStatement)
+        val btnRecharge = findViewById<Button>(R.id.btnRecharge)
+        val btnCard = findViewById<Button>(R.id.btnCard)
+
+        btnStatement.setOnClickListener {
+            val intent = Intent(this, StatementActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRecharge.setOnClickListener {
+            val intent = Intent(this, RechargeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCard.setOnClickListener {
+            val intent = Intent(this, CardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/RechargeActivity.kt
+++ app/src/main/java/com/example/myapplication/RechargeActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RechargeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recharge)
+
+        val btnGoBack = findViewById<Button>(R.id.btnGoBack)
+
+        btnGoBack.setOnClickListener {
+            finish()
+        }
     }
 }
