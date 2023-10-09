7eb1e3d6b56a1b11b4a3b4343ed2e1b6fa3cc8ac
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2023-10-09 20:14:48 -0300

    chore: changing jhon doe to katia doe

--- app/src/main/java/com/example/myapplication/services/CardService.kt
+++ app/src/main/java/com/example/myapplication/services/CardService.kt
@@ -4,6 +4,6 @@ import com.example.myapplication.models.Card
 
 class CardService {
     fun getCard(): Card {
-        return Card("1", "1234 5678 9000", "John Doe", "123")
+        return Card("1", "1234 5678 9000", "Katia Doe", "123")
     }
 }
